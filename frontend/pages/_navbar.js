57d7ad3cc55833f1c6c2618a732efb999c71bd52
Dark Reaper <binarshadsaad@gmail.com>
2023-01-23 23:42:26 +0500

    third commit

--- frontend/pages/_navbar.js
+++ frontend/pages/_navbar.js
@@ -1,17 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Link from 'next/link'
+import Head from 'next/head';
 
-export default function Navbar() {
+
+function ListItem(props){
     return (
-        <nav className="fixed z-10 left-0 top-0 w-16 h-full py-5 shadow-md bg-white flex flex-col justify-between items-center space-y-10">
-            <i class="fa-solid fa-bars text-2xl" />
-            <div className="w-full h-1/4 flex flex-col justify-around items-center">
-            <Link href='/' ><i class="fa-solid fa-magnifying-glass"/></Link>
-            <Link href='/' ><span className="material-symbols-rounded"> local_mall </span></Link>
-            <Link href='/' ><span class="material-symbols-rounded">person</span></Link>
+        <li className='list_item transition-all' ><Link href='#' className='flex justify-between px-5 border-b-2 border-white hover:border-yellow-600 transition-all' >{props.value}<i className="material-symbols-outlined text-lg translate-y-1 transition-all">chevron_right</i></Link></li>
+    )
+}
+export default function Navbar() {
+    const [nav, setNav] = useState('')
+    const [bars, setBars] = useState('')
+    const [menu, setMenu] = useState('-translate-x-full')
+    const handleMenu = () => {
+        if (bars === '') {
+            setBars('open')
+            setMenu('')
+            setNav('lg:opacity-0 lg:pointer-events-none')
+        }
+        if (bars === 'open') {
+            setNav('')
+            setBars('')
+            setMenu('-translate-x-full')
+        }
+    }
+    return (
+        <>
+            <nav className={` ${nav} border fixed z-30 bottom-8 left-1/2 -translate-x-1/2 lg:translate-x-0 lg:left-0 lg:top-0 lg:rounded-none rounded-full w-4/5 lg:w-[5.4%] h-[8%] lg:h-full lg:py-5 shadow-md bg-white bg-opacity-60 backdrop-blur flex lg:flex-col lg:justify-between items-center transition duration-700 lg:space-y-10`}>
+                <Link href='#' onClick={handleMenu} className={`hidden lg:block`}>
+                    <div className={`${bars} menu btn6`}>
+                        <div className="icon"></div>
+                    </div>
+                </Link>
+                <div className="w-full h-full lg:h-1/3 text-gray-900 flex lg:flex-col justify-around items-center">
+                    <Link href='#' className='lg:hidden flex justify-center items-center w-[20%] h-3/4 rounded-full bg-gradient-to-r ' onClick={handleMenu} ><i className="material-symbols-outlined text-[1.6rem">menu</i></Link>
+                    <Link href='/' className=' flex justify-center items-center w-[20%] h-3/4 rounded-full bg-gradient-to-r ' ><i className="material-symbols-outlined text-[1.6rem]">search</i></Link>
+                    <Link href='/' className=' flex justify-center items-center w-[20%] h-3/4 rounded-full bg-gradient-to-r ' ><i className="material-symbols-outlined text-[1.6rem]">local_mall</i></Link>
+                    <Link href='/' className=' flex justify-center items-center w-[20%] h-3/4 rounded-full bg-gradient-to-r ' ><i className="material-symbols-outlined text-[1.6rem]">person</i></Link>
+                </div>
+                <i className='hidden lg:block' />
+            </nav>
+
+            <div className={` ${menu} w-full lg:w-[25%] h-full fixed m-0 left-0 top-0 z-10 transition duration-700 bg-white`}>
+                <div className="border-b-2 w-full h-1/5 flex flex-col">
+                    <div className="flex justify-end items-center w-full h-1/2 px-7">
+                        <Link href='#' onClick={handleMenu} className=''>
+                            <div className={`${bars} menu btn6`}>
+                                <div className="icon"></div>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="flex justify-between items-center w-full h-1/2 p-5">
+                        <Link href='/login' className="flex font_futuraLT"><i className="material-symbols-outlined text-[1.6rem]">person</i>Login</Link>
+                        <span className='flex space-x-5' ><Link href='/login' className="flex font_futuraLT"><i className="material-symbols-outlined text-[1.6rem]">search</i></Link><Link href='/login' ><i className="material-symbols-outlined text-[1.6rem]">local_mall</i></Link></span>
+                    </div>
+                </div>
+                <div className="w-full h-3/5">
+                    <ul className=' list-none text-lg my-5 space-y-5 font_futuraLT' >
+                        <ListItem value='New In' />
+                        <ListItem value='Men' />
+                        <ListItem value='Women' />
+                        <ListItem value='Eyewear' />
+                        <ListItem value='Kids' />
+                        <ListItem value='Gifts Selection' />
+                        <ListItem value='Our Essentials' />
+                    </ul>
+                </div>
+                <div className="w-full h-1/5 border-t-2"></div>
             </div>
-            {/* <i class="fa-solid fa-arrow-right-from-bracket text-white"></i> */}
-            <span></span>
-        </nav>
+        </>
     )
 }
--- frontend/pages/giftcard.js
+++ frontend/pages/giftcard.js
@@ -6,19 +6,21 @@ import giftBanner from '../public/giftbanner.png'
 
 export default function Giftcard() {
     return (
-        <main className="bg-gray-100 ">
+        <>
+            <main className="w-full h-full bg-gray-100 transition duration-700">
             <Navbar />
-            <section className=" w-full pl-16 p-5 flex flex-col justify-center items-center space-y-5 ">
-                <div className=' w-11/12 relative rounded-sm'>
-                    <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-xl lg:text-6xl font_futuraLT ">Urban Gift Cards</h1>
-                    <Image src={giftBanner} className=' rounded-sm' />
-                </div>
-                <div className="w-11/12 border- border-red-800 cards flex flex-col md:flex-row justify-around itmes-center  p-5 h-auto">
-                    <Card sizeClasses='w-2/5 h-2/5' />
-                    <Card sizeClasses='w-2/5 h-2/5' />
-                </div>
-                <div className="w-2/5 "></div>
-            </section>
-        </main>
+                <section className=" w-full lg:pl-[7%] absolute right-0 top-0 p-7 pb-20 lg:pb-0 flex flex-col justify-center items-center space-y-5 transition duration-700">
+                    <div className={`w-full lg:w-11/12 h-[40vh] lg:h-auto relative mx-auto rounded-sm bg_giftbanner bg-no-repeat bg-cover bg-right`}>
+                        <h1 className="w-full text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[6vw] lg:text-[4vw] font_futuraLT ">Urban Gift Cards</h1>
+                        <Image src={giftBanner} alt='' className='hidden lg:block rounded-sm' />
+                    </div>
+                    <div className="w-full lg:w-11/12 p-5 flex flex-col lg:flex-row justify-around items-center space-y-6 lg:space-y-0">
+                        <Card value='Add a personalized messgae, we deliver your gift via email.' sizeClasses='w-full h-72 md:w-1/2 lg:w-2/5 md:h-1/2' />
+                        <Card value='Pick one out and we’ll mail it to the lucky recipent.' sizeClasses='w-full h-72 md:w-1/2 lg:w-2/5 md:h-2/5' />
+                    </div>
+                    <div className="w-2/5 "></div>
+                </section>
+            </main>
+        </>
     )
 }
