58dfbff50b96f88adbf8ea6ae1b0062e54e05cd5
Dark Reaper <binarshadsaad@gmail.com>
2023-01-25 22:12:34 +0500

    removed eslint

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4a822326a9e57235"; }
+/******/ 		__webpack_require__.h = function() { return "174071926cf02e14"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/.next/static/webpack/webpack.8e47fda0d605109b.hot-update.js
+++ frontend/.next/static/webpack/webpack.8e47fda0d605109b.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "38f5ec637d396b0f"; }
+/******/ 	__webpack_require__.h = function() { return "1ec7bcb6498691b4"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
