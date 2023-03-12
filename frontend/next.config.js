2bbd12621b0563cf1ffd3a2480a6e168061cf41c
Faizan Sohail <117750943+Dark-Reaper6@users.noreply.github.com>
2023-03-12 18:36:57 +0500

    12-3-2023

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,12 +10,8 @@ const nextConfig = {
     HOST: "https://urbanfits.vercel.app",
     SECRET_KEY: "MuhammadBilawalAshrafOwnsUrbanFisBrand",
     // Google client credentials
-    GOOGLE_CLIENT_ID: "829021307807-g882f28vb65s9nn40gs4rk4kptga7jqq.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
     // Email SMTP credentials
     SMTP_HOST: "smtp.sendgrid.net",
-    SMTP_USER: "apikey",
-    SMTP_PASSWORD: "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",
     SMTP_SENDER_EMAIL: "dark_reaper6@outlook.com",
   },
   async headers() {
