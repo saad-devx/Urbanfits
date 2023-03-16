e311e5f204c0172ba7ba0007cf34a189e09e9dc6
Dark Reaper <binarshadsaad@gmail.com>
2023-03-16 14:49:53 +0500

    16-3-2023

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
   },
   env: {
     MONGO_URI: "mongodb+srv://darkreaper:s19114666d@cluster0.eyxeosm.mongodb.net/Urbanfits?retryWrites=true&w=majority",
-    HOST: "http://localhost:3000",
-    // HOST: "https://urbanfits.vercel.app",
+    // HOST: "http://localhost:3000",
+    HOST: "https://urbanfits.vercel.app",
     SECRET_KEY: "MuhammadBilawalAshrafOwnsUrbanFisBrand",
     // Google client credentials
     GOOGLE_CLIENT_ID: "",
@@ -18,19 +18,19 @@ const nextConfig = {
     SMTP_PASSWORD: "",
     SMTP_SENDER_EMAIL: "",
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       // matching all API routes
-  //       source: "/api/:path*",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Credentials", value: "true" },
-  //         { key: "Access-Control-Allow-Origin", value: "*" },
-  //         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-  //         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-  //       ]
-  //     }
-  //   ]
-  // }
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 }
 module.exports = nextConfig
