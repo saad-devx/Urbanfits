fd7903eab2b92fdfee61b17b9b1ce0a416a9b312
Dark Reaper <binarshadsaad@gmail.com>
2023-03-21 01:08:43 +0500

    21-3-2023

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2ab35945ba58c058"; }
+/******/ 		__webpack_require__.h = function() { return "ce13b06068d1bdd2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/components/cart.js
+++ frontend/components/cart.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useCart } from "react-use-cart";
-import { useRouter } from 'next/router';
 import Accordians from './accordians';
 import LinkBtn from '@/components/buttons/link_btn';
 import Button from './buttons/simple_btn';
@@ -12,20 +11,28 @@ import EmptyCartVector from "../public/emptyCart.svg"
 const image1 = 'https://images.unsplash.com/photo-1551377293-17f3c11c4768?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
 
 // Cart item function
-function CartItem (props)  {
-    const router = useRouter()
-    const product = props.product
-    // confguring the Quantity conter of the prodcut
-    const [quantity, setQuantity] = useState(1)
-    const changeQuantity = (e) => {
-        let name = e.target.getAttribute("name")
-        if (name === "decrement" && quantity === 1) return
-        if (name === "increment" && quantity === props.stock ? props.stock : 1) return
-        if (name === "decrement") return setQuantity(quantity - 1)
-        if (name === "increment") return setQuantity(quantity + 1)
-    }
+function CartItem(props) {
+    const { product } = props
     //Setting up the cart functions
     const { updateItemQuantity, removeItem } = useCart()
+    // confguring the Quantity conter of the prodcut
+    const [quantity, setQuantity] = useState(product.quantity)
+    // updateItemQuantity(product.id, product.quantity)
+    const changeQuantity = (e, id) => {
+        // updateItemQuantity(product.id, product.quantity)
+        let name = e.target.getAttribute("name")
+        if (name === "decrement" && quantity === 1) return
+        if (name === "increment" && quantity === product.stock) return
+        if (name === "decrement"){
+            setQuantity(quantity - 1)
+            updateItemQuantity(id, quantity-1)
+        }
+        if (name === "increment"){
+            setQuantity(quantity + 1)
+            updateItemQuantity(id, quantity+1)
+        }
+    }
+
     return (
         <li key={props.li_key} className="relative mb-3 bg-white card_boxshadow w-full h-64 md:h-80 flex justify-between items-center rounded-xl md:rounded-3xl p-5">
             <div className=" w-[35%] md:w-1/3 h-3/5 md:h-full">
@@ -34,20 +41,21 @@ function CartItem (props)  {
             <div className="w-4/6 md:w-2/5 flex flex-col items-end">
                 <h3 className="w-full lg:text-2xl text-end">{product.name}</h3>
                 <div className="w-3/4 h-auto text-sm md:text-base my-5 md:my-3 font_futuraLTlite">
-                    <span key={1} className="w-full md:mb-2 mx-auto flex justify-between"><small>Color</small> <small>{props.color}</small></span>
-                    <span key={2} className="w-full md:mb-2 mx-auto flex justify-between"><small>Size</small> <small>{props.size}</small></span>
-                    <span key={3} className="w-full md:mb-2 mx-auto flex justify-between"><small>Quantity</small> <small>{props.quantity}</small></span>
-                    <span key={4} className="w-full md:mb-2 mx-auto flex justify-between"><small>Price</small> <small>${props.price}</small></span>
-                    <span key={5} className="w-full md:mb-2 mx-auto flex justify-between"><small>Discount</small> <small>{props.discount ? props.discount : 0}</small></span>
-                    <span key={6} className="w-full md:mb-2 mx-auto flex justify-between"><small>Sale Price</small> <small>${props.price + 0}</small></span>
+                    <span key={1} className="w-full md:mb-1 mx-auto flex justify-between"><small>Color</small><small>{product.color}</small></span>
+                    <span key={2} className="w-full md:mb-1 mx-auto flex justify-between"><small>Size</small><small>{product.size}</small></span>
+                    <span key={3} className="w-full md:mb-1 mx-auto flex justify-between"><small>Quantity</small><small>{quantity}</small></span>
+                    <span key={4} className="w-full md:mb-1 mx-auto flex justify-between"><small>Price(per piece)</small><small>${product.price}</small></span>
+                    <span key={5} className="w-full md:mb-1 mx-auto flex justify-between"><small>Discount</small><small>{product.discount ? product.discount : 0}%</small></span>
+                    {/* <span key={7} className="w-full md:mb-1 mx-auto flex justify-between"><small>Sale Price</small><small>${product.price * quantity.toFixed(3)}</small></span> */}
+                    <span key={7} className="w-full md:mb-1 mx-auto flex justify-between"><small>Sale Price</small><small>${props.get3dpNumber(product.price * quantity)}</small></span>
                 </div>
                 <div className=" text-sm md:text-base w-full flex justify-between">
-                    <small><Link href={`/products/product/${product._id}`} onClick={()=>{props.toggleCart()}} >Edit<i className="fa-regular fa-pen-to-square ml-1"></i></Link></small>
-                    <small onClick={() => { removeItem(product._id) }} ><button>Remove <i className="fa-solid fa-trash text-gray-800 ml-1"></i></button></small>
+                    <small><Link href={`/products/product/${product.id}`} onClick={() => { props.toggleCart() }} >Edit<i className="fa-regular fa-pen-to-square ml-1"></i></Link></small>
+                    <small onClick={() => { removeItem(product.id) }} ><button>Remove <i className="fa-solid fa-trash text-gray-800 ml-1"></i></button></small>
                     <span className="w-20 md:w-24 flex justify-between">
-                        <span onClick={changeQuantity} name="decrement" className="fa-solid fa-circle-minus text-lg md:text-xl cursor-pointer active:-translate-x-2 transition-all text-gray-300"></span>
+                        <span onClick={(e) => { changeQuantity(e, product.id) }} name="decrement" className="fa-solid fa-circle-minus text-lg md:text-xl cursor-pointer active:-translate-x-2 transition-all text-gray-300"></span>
                         <input type="number" readOnly className='w-2/5 md:w-3/5 h-auto text-sm text-center md:mx-3 border-none outline-none pointer-events-none' value={quantity} />
-                        <span onClick={() => updateItemQuantity(product.id, product.stock + 1)} name="increment" className="fa-solid fa-circle-plus text-lg md:text-xl cursor-pointer active:translate-x-2 transition-all text-gray-300"></span>
+                        <span onClick={(e) => { changeQuantity(e, product.id) }} name="increment" className="fa-solid fa-circle-plus text-lg md:text-xl cursor-pointer active:translate-x-2 transition-all text-gray-300"></span>
                     </span>
                 </div>
             </div>
@@ -57,7 +65,11 @@ function CartItem (props)  {
 
 export default function Cart(props) {
     // Setting up the Cart functions
-    const { isEmpty, totalUniqueItems, items } = useCart()
+    const { isEmpty, totalUniqueItems, items, cartTotal } = useCart()
+    // function to get rounded off number upto 3 decimal places
+    const get3dpNumber = (num)=>{
+        return num.toFixed(3)
+    }
 
     return (
         <>
@@ -74,15 +86,16 @@ export default function Cart(props) {
                                 <div className="w-full lg:w-[70%] mb-3">
                                     <span className="w-full flex justify-between border-b border-b-gray-300 mb-5"> <h5>Shopping Bag({totalUniqueItems})</h5> <button onClick={props.toggleCart}><i className="fa-solid fa-arrow-left mr-2"></i>Back</button> </span>
                                     {items.map((product, index) => {
-                                        return <CartItem li_key={index} product={product} size={product.size[0]} toggleCart={props.toggleCart} />
+                                        return <CartItem li_key={product.id} product={product} size={product.size[0]} toggleCart={props.toggleCart} get3dpNumber={get3dpNumber} />
                                     })}
                                 </div>
                                 <div className="details w-full lg:w-[30%] m-0 space-y-3">
                                     <h3 className="text-2xl mb-5">Order Summary</h3>
                                     <div className="w-full h-auto p-4 rounded-2xl bg-white items-center card_boxshadow">
-                                        <span className="w-full my-3 mx-auto flex justify-between"><small>Subtotal</small> <small>$0</small></span>
-                                        <span className="w-full my-3 mx-auto flex justify-between"><small>Shipping</small> <small>$0</small></span>
-                                        <span className="w-full my-3 mx-auto flex justify-between"><small>Sales Tax</small> <small>$0</small></span>
+                                        <span className="w-full my-3 mx-auto flex justify-between"><small>Subtotal</small> <small>${get3dpNumber(cartTotal)}</small></span>
+                                        <span className="w-full my-3 mx-auto flex justify-between"><small>Shipping</small> <small>${items[0].shipping_fee}</small></span>
+                                        <span className="w-full my-3 mx-auto flex justify-between"><small>Sales Tax</small> <small>${0}</small></span>
+                                        <span className="w-full my-3 mx-auto flex justify-between"><small>Total</small> <small>${parseFloat(cartTotal + items[0].shipping_fee).toFixed(3)}</small></span>
                                         <LinkBtn href="/checkout/step1" onclick={props.toggleCart} classes="w-full" >Checkout</LinkBtn>
                                     </div>
                                     {/* Accordian */}
