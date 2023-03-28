9a757de89a2729a64dd3e8643137f19da4233d93
Dark Reaper <binarshadsaad@gmail.com>
2023-03-29 01:38:57 +0500

    28-3-2023

---
+++ urban-fits/tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      width: {
+        '35pr': '35%',
+        '90pr': '90%',
+        '70pr': '70%',
+        '48pr': '48%',
+        '30pr': '30%',
+        '20pr': '20%',
+        '60vw': '60vw',
+        '90vw': '90vw',
+      },
+      borderRadius: {
+        '2rem': '2rem',
+      },
+      height: {
+        '35pr': '35%',
+        '60vh': '60vh',
+        '80vh': '80vh',
+        '87vh': '87vh',
+        '80pr': '80%',
+        '90vh': '90vh',
+      },
+      backgroundImage: {
+        'gold' : "linear-gradient(180deg, #FAE892 0%, #B3903E 100%)",
+        'gold-land' : "linear-gradient(90deg, #FAE892 0%, #B3903E 100%)",
+      }
+    }
+  },
+  plugins: [],
+}
