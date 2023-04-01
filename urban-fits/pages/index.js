e2ecad91c6ae2f6a786fc11be2b6d090099319f6
Dark Reaper <binarshadsaad@gmail.com>
2023-04-02 01:42:51 +0500

    1-4-2023

--- urban-fits/pages/index.js
+++ urban-fits/pages/index.js
@@ -86,11 +86,11 @@ export default function Home() {
                             <PicCard h1="NEW IN" h2="READY TO WEAR" btnValue="SHOP MEN" img={image3} />
                         </section>
                         {/* second carousel */}
-                        <section className="relative bg_metal_gold w-full h-60vh lg:h-87vh min-h-[500px] lg:px-10 lg:pr-0 flex flex-col-reverse lg:flex-row items-center justify-center font_gotham overflow-hidden">
+                        <section className="relative bg_metal_gold w-full h-[70vh] lg:h-87vh min-h-[300px] lg:px-10 lg:pr-0 flex flex-col-reverse lg:flex-row items-center justify-center font_gotham overflow-hidden">
                             <div className="hidden lg:flex left-0 w-full lg:w-[35vw] lg:h-full md:pl-10 mb-3 lg:m-0 leading-7 flex-col justify-center items-start">
                                 <h2 className="text-xl md:text-3xl lg:text-[32px] word-wrap font_gotham_medium leading-tight">URBAN<br />NEW ARRIVAL</h2>
                             </div>
-                            <div className="h-full w-full lg:w-[65vw] rounded-3xl flex items-center">
+                            <div className="h-full w-full lg:w-[65vw] flex items-center">
                                 <HomeCarousel2 />
                             </div>
                         </section>
