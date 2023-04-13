2c16e6c2486b90f82814ea94369331cbd286c268
Dark Reaper <binarshadsaad@gmail.com>
2023-04-13 15:07:07 +0500

    13-4-2023

--- urban-fits/components/accordians/accordian.js
+++ urban-fits/components/accordians/accordian.js
@@ -12,9 +12,9 @@ export default function Accordian (props) {
                 <div className="text-sm font_gotham_medium text-black tracking-widest transition ease duration-700">{props.title}</div>
                 <i className={`${open ? '-rotate-180' : null} fa-solid fa-caret-down transform transition ease duration-500`}></i>
             </div>
-            <div className={` ${open ? 'max-h-screen mt-5' : 'max-h-0'} text-sm leading-5 rounded overflow-y-hidden scrollbar_x ease duration-700`}>
+            <div className={` ${open ? 'max-h-screen mt-5' : 'max-h-0'} relative whitespace-nowrap max-w-full text-sm leading-5 rounded overflow-y-hidden scrollbar_x ease duration-700`}>
                 <div className="flex items-center mb-3 py-2">
-                    <div className='font_gotham_light text-sm text-black'>{props.children}</div>
+                    <div className='inline font_gotham_light text-sm text-black'>{props.children}</div>
                 </div>
             </div>
         </div>
