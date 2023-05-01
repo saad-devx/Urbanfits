d001ac6fd05f6ce936777fd1f1357f7050dd002f
Dark Reaper <binarshadsaad@gmail.com>
2023-05-01 19:31:52 +0500

    1-5-2023

--- urban-fits/components/listingShop_section.js
+++ urban-fits/components/listingShop_section.js
@@ -1,27 +1,39 @@
 import React from 'react'
 import Image from 'next/image'
-
-const Vector = (props) => {
-    return <svg className={`${props.classes} absolute top-12 left-44 2xl:left-60`} width="582" height="102" viewBox="0 0 582 102" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0.978516 1H580.621V101.5" stroke={props.fill} stroke-width="2" />
-    </svg>
-}
+import blackThemeBg from '@/public/listing banners/listingbg1.jpg'
+import whiteThemeBg from '@/public/listing banners/listingbg2.jpg'
 
 export default function ListingShopSection(props) {
-    return (
+    if (window.matchMedia('(min-width: 768px)').matches) return (
         <div className={`w-full h-60 lg:h-[430px] hidden lg:flex relative justify-between ${props.classes}`}>
             <div className={`lg:w-3/5 h-full z-10 ${props.whiteTheme ? 'bglisting_white' : 'bglisting_black'}`}>
-                <svg className='absolute top-12 left-44 2xl:left-60' width="582" height="102" viewBox="0 0 582 102" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0.978516 1H580.621V101.5" stroke={props.whiteTheme ? 'black' : 'white'} stroke-width="2" />
-                </svg>
-                <div className="absolute left-32 2xl:left-48 top-1/2 -translate-y-1/2 flex flex-col">
-                    <h2 className={`font_gotham_black lg:text-[40px] mb-5 ${props.whiteTheme ? 'text-black' : 'text-white'}`}>SHOPPING WITHOUT LIMITS.</h2>
+                <div className="absolute left-[9%] top-1/2 -translate-y-1/2 flex flex-col">
+                    <svg className='absolute top-0 left-[9%]' width="582" height="102" viewBox="0 0 582 102" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0.978516 1H580.621V101.5" stroke={props.whiteTheme ? 'black' : 'white'} stroke-width="2" />
+                    </svg>
+                    <h2 className={`font_gotham_black lg:text-[40px] my-5 ${props.whiteTheme ? 'text-black' : 'text-white'}`}>SHOPPING WITHOUT LIMITS.</h2>
                     <p className={`mb-8 font_gotham text-[10px] lg:text-[23px] ${props.whiteTheme ? 'text-black' : 'text-white'}`}>You can choose the best option for you, and it does <br /> not matter whether you are in UAE or USA. We will <br /> deliver your purchase anywhere!</p>
                     <button className={`w-[168px] py-3 border-2 ${props.whiteTheme ? 'border-black text-black' : 'border-white text-white'} font_gotham_black text-lg`}>SHOP NOW</button>
                 </div>
             </div>
             <div className="absolute top-0 right-0 lg:w-3/5 h-full">
-                <Image width={1000} height={450} src={props.img} alt='shopping section cover image' className='h-full float-right object-cover object-right' />
+                <Image width={1000} height={450} src={props.whiteTheme? whiteThemeBg: blackThemeBg} alt='shopping section cover image' className='h-full float-right object-cover object-right' />
+            </div>
+        </div>
+    )
+
+    // For Mobile and screens
+    if (window.matchMedia('(max-width: 768px)').matches) return (
+        <div className={`w-screen h-60 flex relative ${props.whiteTheme ? null : '-left-5'} justify-between ${props.classes}`}>
+            <div className={`w-3/4 h-full z-10 ${props.whiteTheme ? 'bglisting_white' : 'bglisting_black'}`}>
+                <div className="absolute left-[9%] top-1/2 -translate-y-1/2 max-w-[60%]">
+                    <h2 className={`inline-block font_gotham_black text-xl pt-3 pr-3 pb-1 mb-2 border-2 border-transparent ${props.whiteTheme ? 'text-black border-t-black border-r-black' : 'text-white border-t-white border-r-white'}`}>SHOPPING <br /> WITHOUT LIMITS.</h2>
+                    <p className={`mb-4 font_gotham text-[10px] ${props.whiteTheme ? 'text-black' : 'text-white'}`}>You can choose the best option for you, and it does not matter whether you are in UAE or USA. We will deliver your purchase anywhere!</p>
+                    <button className={`w-32 py-2.5 border-2 ${props.whiteTheme ? 'border-black text-black' : 'border-white text-white'} font_gotham_medium text-xs tracking-wider`}>SHOP NOW</button>
+                </div>
+            </div>
+            <div className="absolute top-0 right-0 h-full">
+                <Image width={1000} height={450} src={props.whiteTheme? whiteThemeBg: blackThemeBg} alt='shopping section cover image' className='h-full float-right object-cover object-left' />
             </div>
         </div>
     )
--- urban-fits/pages/user/orders/orders.js
+++ urban-fits/pages/user/orders/orders.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import OrdersPage from './index'
+import Invoice from '@/components/modals/invoice'
 import Image from 'next/image'
 import shirt_img from '../../../public/card imgs/card img4.png'
 
 const OrderCard = (props) => {
+    const [invoice, setInvoice] = useState(false)
+    const toggleInvoice = ()=>{
+        if(!invoice) return setInvoice(true)
+        if(invoice) return setInvoice(false)
+    }
     return (
+        <>
+        <Invoice toggleInvoice={toggleInvoice} show={invoice} />
         <div className=" w-full h-48 md:h-52 my-3 flex flex-col items-start rounded-xl overflow-clip">
             <div className="bg-gray-50 w-full h-[30%] px-2 md:px-5 py-2 font_gotham_light text-[10px] md:text-xs flex justify-between">
                 <div className="w-2/5 h-full flex justify-between">
@@ -18,9 +26,9 @@ const OrderCard = (props) => {
                 <div className='w-auto h-full flex flex-col justify-between items-end space-y-2 font_gotham'>
                     <h6>Order: # <code className='font_gotham_light' >{props.order_number}</code></h6>
                     <div className="w-full flex justify-between">
-                        <p className="underline cursor-pointer">View Order Details</p>
+                        <button className="underline cursor-pointer">View Order Details</button>
                         <span className="bg-black w-[1px] h-3 md:h-4 translate-y-1"></span>
-                        <p className="underline cursor-pointer">Invoice</p>
+                        <button onClick={toggleInvoice} className="underline cursor-pointer">Invoice</button>
                     </div>
                 </div>
             </div>
@@ -39,17 +47,20 @@ const OrderCard = (props) => {
                 </span>
             </div>
         </div>
+        </>
     )
 }
 
 export default function orders() {
     return (
-        <OrdersPage>
-            <h2 className="text-xs md:text-sm">Order Placed in 2023</h2>
-            <OrderCard product_title="Urban T-shirt with logo Design" img={shirt_img} order_date="8 Jan 2023" price="$78.00" order_number="406-0406900-597108" />
-            <OrderCard product_title="Urban T-shirt with logo Design" img={shirt_img} order_date="8 Jan 2023" price="$78.00" order_number="406-0406900-597108" />
-            <OrderCard product_title="Urban T-shirt with logo Design" img={shirt_img} order_date="8 Jan 2023" price="$78.00" order_number="406-0406900-597108" />
-            <OrderCard product_title="Urban T-shirt with logo Design" img={shirt_img} order_date="8 Jan 2023" price="$78.00" order_number="406-0406900-597108" />
-        </OrdersPage>
+        <>
+            <OrdersPage>
+                <h2 className="text-xs md:text-sm">Order Placed in 2023</h2>
+                <OrderCard product_title="Urban T-shirt with logo Design" img={shirt_img} order_date="8 Jan 2023" price="$78.00" order_number="406-0406900-597108" />
+                <OrderCard product_title="Urban T-shirt with logo Design" img={shirt_img} order_date="8 Jan 2023" price="$78.00" order_number="406-0406900-597108" />
+                <OrderCard product_title="Urban T-shirt with logo Design" img={shirt_img} order_date="8 Jan 2023" price="$78.00" order_number="406-0406900-597108" />
+                <OrderCard product_title="Urban T-shirt with logo Design" img={shirt_img} order_date="8 Jan 2023" price="$78.00" order_number="406-0406900-597108" />
+            </OrdersPage>
+        </>
     )
 }
