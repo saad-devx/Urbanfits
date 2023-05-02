cc46aa48906ec3259a8b450ea282b2c28c0a3c66
Dark Reaper <binarshadsaad@gmail.com>
2023-05-03 01:20:57 +0500

    3-5-2023

--- urban-fits/pages/checkout/step1.js
+++ urban-fits/pages/checkout/step1.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { useRouter } from 'next/router';
+import { useCart } from "react-use-cart";
 import jwt from 'jsonwebtoken';
 import Navbar from '@/components/navbar';
 import Loader from '@/components/loader';
@@ -153,6 +154,7 @@ export default function Checkout1(props) {
         else return
     }
 
+    const { isEmpty, totalUniqueItems, items, cartTotal, emptyCart } = useCart()
     return (
         <>
             {loader}
@@ -316,31 +318,36 @@ export default function Checkout1(props) {
                             </form>
                         </div>
                         <div className="details w-full lg:w-[31%] m-0 space-y-3">
-                            <h3 className="text-2xl md:text-[28px] font_gotham_medium tracking-whidest mb-5">ORDER SUMMARY</h3>
-                            <div className=" hidden lg:flex relative mb-3 p-6 bg-white card_boxshadow w-full max-h-screen flex-col justify-between items-center rounded-lg md:rounded-xl">
-                                <h3 className="self-start mb-3 font_gotham_medium text-sm md:text-base tracking-wide lg:tracking-widest text-left">PRODUCT TITLE HERE</h3>
-                                <div className="flex justify-between items-center">
-                                    <div className=" w-[35%] md:w-1/3">
-                                        <Image width={640} height={853} src={shirt_img} alt="Urban images" className="w-full h-full rounded-md md:rounded-lg object-cover object-top" ></Image>
-                                    </div>
-                                    <div className="w-1/2 h-auto text-[10px] md:text-xs my-5 md:my-3 space-y-1 md:space-y-1.5 font_gotham_light">
-                                        <div key={1} className="w-full mx-auto flex justify-between"><span className='font_gotham_medium'>Color:</span> <span>{props.color}</span></div>
-                                        <div key={2} className="w-full mx-auto flex justify-between"><span className='font_gotham_medium'>Size:</span> <span>{props.size}</span></div>
-                                        <div key={3} className="w-full mx-auto flex justify-between"><span className='font_gotham_medium'>Quantity:</span> <span>{props.quantity}</span></div>
-                                        <div key={4} className="w-full mx-auto flex justify-between"><span className='font_gotham_medium'>Price:</span> <span>${props.price}</span></div>
-                                        <div key={5} className="w-full mx-auto flex justify-between"><span className='font_gotham_medium'>Discount:</span> <span>{props.discount ? props.discount : 0}</span></div>
-                                        <div key={6} className="w-full mx-auto flex justify-between"><span className='font_gotham_medium'>Sale Price:</span> <span>${props.price + 0}</span></div>
-                                    </div>
-                                </div>
+                            <h3 className="text-2xl md:text-[28px] font_gotham_medium tracking-whidest mb-5">ORDER SUMMARY {totalUniqueItems}</h3>
+                            <div className="flex relative mb-3 p-6 bg-white card_boxshadow w-full max-h-screen flex-col justify-between items-center rounded-lg md:rounded-xl">
+
+                                {items.map((item, i) => {
+                                    console.log(i, item)
+                                    return <>
+                                        <h3 className="self-start mb-3 font_gotham_medium text-sm md:text-base tracking-wide lg:tracking-widest text-left">{item?.name.toUpperCase()}</h3>
+                                        <div className="w-full mb-4 flex justify-between items-center">
+                                            <div className="w-[125px] h-[125px] rounded-md md:rounded-lg overflow-hidden">
+                                                <Image width={640} height={640} src={item.images[0].url} alt="Urban images" className="w-full h-full object-cover object-top" />
+                                            </div>
+                                            <div className="w-1/2 h-auto text-[10px] md:text-xs my-5 md:my-3 space-y-1 md:space-y-1.5 font_gotham_light">
+                                                <div key={1} className="w-full mx-auto flex justify-between"><span className='font_gotham_medium'>Color:</span> <span>{item.color}</span></div>
+                                                <div key={2} className="w-full mx-auto flex justify-between"><span className='font_gotham_medium'>Size:</span> <span>{item.size}</span></div>
+                                                <div key={3} className="w-full mx-auto flex justify-between"><span className='font_gotham_medium'>Quantity:</span> <span>{item.quantity}</span></div>
+                                                <div key={4} className="w-full mx-auto flex justify-between"><span className='font_gotham_medium'>Price:</span> <span>${item.price}</span></div>
+                                                <div key={5} className="w-full mx-auto flex justify-between"><span className='font_gotham_medium'>Discount:</span> <span>{item.discount ? props.discount : 0}%</span></div>
+                                                <div key={6} className="w-full mx-auto flex justify-between"><span className='font_gotham_medium'>Sale Price:</span> <span>${item.price + 0}</span></div>
+                                            </div>
+                                        </div>
+                                    </>
+                                })}
                                 <div className="w-full h-auto my-5 md:my-3 font_gotham_medium text-xs md:text-sm">
-                                    <div key={1} className="w-full mx-auto flex justify-between"><span>Subtotal</span> <span>{props.color}</span></div>
-                                    <div key={2} className="w-full mx-auto flex justify-between"><span>Size</span> <span>{props.size}</span></div>
-                                    <div key={3} className="w-full mx-auto flex justify-between"><span>Quantity</span> <span>{props.quantity}</span></div>
-                                    <div key={4} className="w-full mx-auto flex justify-between"><span>Price</span> <span>${props.price}</span></div>
+                                    <div key={1} className="w-full mx-auto flex justify-between"><span>Subtotal</span> <span>{cartTotal.toFixed(3)}</span></div>
+                                    <div key={2} className="w-full mx-auto flex justify-between"><span>Shipping</span> <span>${3.94}</span></div>
+                                    <div key={2} className="w-full mx-auto flex justify-between"><span>Sales Tax</span> <span>${4.28}</span></div>
                                 </div>
                                 <div className="w-full py-2 flex justify-between font_gotham_medium tracking-widest border-t border-t-gray-200">
                                     <h4 className="text-lg">Total</h4>
-                                    <h4 className="text-lg">$89.78</h4>
+                                    <h4 className="text-lg">${(cartTotal + 3.94 + 4.28).toFixed(3)}</h4>
                                 </div>
                             </div>
                             <Accordians />
