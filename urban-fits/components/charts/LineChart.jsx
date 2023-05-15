4867be4b1f4e4eda61ee72b0d878c2f90ad934fe
sajeel12 <msajeelahmad2001@gmail.com>
2023-05-15 13:22:49 +0500

    15May2023

--- urban-fits/components/charts/LineChart.jsx
+++ urban-fits/components/charts/LineChart.jsx
@@ -8,9 +8,12 @@ import {
   Title,
   Tooltip,
   Legend,
+  Filler
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 // import faker from 'faker';
+
+ChartJS.register(Filler);
 
 ChartJS.register(
   CategoryScale,
@@ -23,16 +26,46 @@ ChartJS.register(
 );
 
 export const options = {
+
   responsive: true,
   plugins: {
     legend: {
-      position: 'top' ,
+      display:false,
+      // position: 'top' ,
     },
-    title: {
+    // title: {
+    //   display: true,
+    //   text: 'Chart.js Line Chart',
+    // },
+  },
+ // Modify the axis by adding scales
+ scales: {
+  // to remove the labels
+  x: {
+    ticks: {
       display: true,
-      text: 'Chart.js Line Chart',
+    },
+
+    // to remove the x-axis grid
+    grid: {
+      drawBorder: false,
+      display: false,
     },
   },
+  // to remove the y-axis labels
+  y: {
+    ticks: {
+      display: false,
+      beginAtZero: true,
+    },
+    // to remove the y-axis grid
+    grid: {
+      drawBorder: true,
+      display: true,
+    },
+  },
+},
+
 };
 
 const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'];
@@ -41,24 +74,25 @@ export const data = {
   labels,
   datasets: [
     {
-        label: 'My First Dataset',
-        data: [65, 59, 80, 10, 56, 55, 5],
+        // label: 'My First Dataset',
+        data: [65, 59, 10, 56, 55, 5, 0],
         fill: true,
-        borderColor: 'rgb(75, 192, 192)',
-        backgroundColor: 'red',
+        backgroundColor: 'rgba(20, 160, 120,0.3)',
+        borderColor: 'rgba(75, 192, 192)',
         tension: 0.3,
     },
     {
-      label: 'Dataset 2',
+      // label: 'Dataset 2',
     //   data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      data: [35, 60, 18],
+      data: [35, 60, 18,0],
       fill: true,
+      backgroundColor: 'rgba(120, 190, 50,0.3)',
       borderColor: 'rgb(53, 162, 235)',
       tension: 0.3,
 
     },
     {
-      label: 'Dataset 2',
+      // label: 'Dataset 2',
     //   data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
       data: [0, 35, 60, 18, 0],
       fill: true,
--- urban-fits/pages/admin/dashboard.jsx/index.jsx
+++ urban-fits/pages/admin/dashboard.jsx/index.jsx
@@ -2,18 +2,151 @@ import React from 'react'
 import Sidebaradmin from '../sidebar'
 import CardAdmin from '@/components/cards/cardadmin'
 import LineChart from '@/components/charts/LineChart'
+import { AvatarIcon } from '@/public/sidebaricons/AvatarIcon'
 
 const index = () => {
   return (
     <Sidebaradmin>
 
+      
+        <section className='grid grid-cols-4 gap-[27px] mt-[20px] ' >
+          <div className='flex flex-col gap-[13px]' >
             <CardAdmin>
-                <div className='gird grid-cols-1' >
-
-                    <LineChart/>
-
+              <div className='px-[20px] py-[30px] rounded-[15px] flex justify-between items-center ' >
+                <div className='flex flex-col gap-[8px]' >
+                  <p className=' text-[14px] font-[400] ' >Daily Signup</p>
+                  <p className=' text-[22px] font-[500] ' >1,503</p>
                 </div>
+                <div className='' >
+                  <AvatarIcon fill="#F5F5F5" stroke="black" />
+                </div>
+              </div>
             </CardAdmin>
+            <CardAdmin>
+              <div className='px-[20px] py-[30px] rounded-[15px] flex justify-between items-center ' >
+                <div className='flex flex-col gap-[8px]' >
+                  <p className=' text-[14px] font-[400] ' >Products</p>
+                  <p className=' text-[22px] font-[500] ' >1,255</p>
+                  <p className=' text-[12px] font-[300] ' >In 20 Categories</p>
+                </div>
+                <div className='' >
+                  <AvatarIcon fill="#F5F5F5" stroke="black" />
+                </div>
+              </div>
+            </CardAdmin>
+            
+
+          </div>
+          <div className='flex flex-col gap-[13px]' >
+            <CardAdmin>
+              <div className='px-[20px] py-[30px] rounded-[15px] flex justify-between items-center ' >
+                <div className='flex flex-col gap-[8px]' >
+                  <p className=' text-[14px] font-[400] ' >Daily Visitors</p>
+                  <p className=' text-[22px] font-[500] ' >7,903</p>
+                </div>
+                <div className='' >
+                  <AvatarIcon fill="#F5F5F5" stroke="black" />
+                </div>
+              </div>
+            </CardAdmin>
+            <CardAdmin>
+              <div className='px-[20px] py-[30px] rounded-[15px] flex justify-between items-center ' >
+                <div className='flex flex-col gap-[8px]' >
+                  <p className=' text-[14px] font-[400] ' >Monthly Earning</p>
+                  <p className=' text-[22px] font-[500] ' >$37,903</p>
+                  <p className=' text-[12px] font-[300] ' >Based in your local time</p>
+                </div>
+                <div className='' >
+                  <AvatarIcon fill="#F5F5F5" stroke="black" />
+                </div>
+              </div>
+            </CardAdmin>
+            
+
+          </div>
+
+          <div className='flex flex-col gap-[13px]' >
+            <CardAdmin>
+              <div className='px-[20px] py-[30px] rounded-[15px] flex justify-between items-center ' >
+                <div className='flex flex-col gap-[8px]' >
+                  <p className=' text-[14px] font-[400] ' >Orders</p>
+                  <p className=' text-[22px] font-[500] ' >7,903</p>
+                  <p className=' text-[12px] font-[300] ' >Excluding orders in transit</p>
+                </div>
+                <div className='' >
+                  <AvatarIcon fill="#F5F5F5" stroke="black" />
+                </div>
+              </div>
+            </CardAdmin>
+            <CardAdmin>
+              <div className='px-[20px] py-[30px] rounded-[15px] flex justify-between items-center ' >
+                <div className='flex flex-col gap-[8px]' >
+                  <p className=' text-[14px] font-[400] ' >Monthly Sales</p>
+                  <p className='text-[22px] font-[500]' >400 <span className='text-[12px] font-[300]' >Units</span> </p>  
+                </div>
+                <div className='' >
+                  <AvatarIcon fill="#F5F5F5" stroke="black" />
+                </div>
+              </div>
+            </CardAdmin>
+            
+
+          </div>
+          <div className='flex flex-col gap-[13px]' >
+            <CardAdmin>
+              <div className='px-[20px] py-[30px] rounded-[15px] flex justify-between items-center ' >
+                <div className='flex flex-col gap-[8px]' >
+                  <p className=' text-[14px] font-[400] ' >Daily Revenue</p>
+                  <p className=' text-[22px] font-[500] ' >7,903</p>
+                  <p className=' text-[12px] font-[300] ' >Based in your local time</p>
+                </div>
+                <div className='' >
+                  <AvatarIcon fill="#F5F5F5" stroke="black" />
+                </div>
+              </div>
+            </CardAdmin>
+            <CardAdmin>
+              <div className='px-[20px] py-[30px] rounded-[15px] flex justify-between items-center ' >
+                <div className='flex flex-col gap-[8px]' >
+                  <p className=' text-[14px] font-[400] ' >Cancelled Orders</p>
+                  <p className='text-[22px] font-[500]' > 300 <span className='text-[12px] font-[300]' >Units</span> </p>  
+                </div>
+                <div className='' >
+                  <AvatarIcon fill="#F5F5F5" stroke="black" />
+                </div>
+              </div>
+            </CardAdmin>
+            
+
+          </div>
+        </section>
+
+        <section className='grid grid-cols-6 mt-[30px] ' >
+          <div className='col-span-4' >
+            <CardAdmin>
+              <div className='px-[30px] py-[33.5px] ' >  
+              <LineChart/>
+
+              </div>
+            </CardAdmin>
+          </div>
+          <div className='col-span-2' >
+            <CardAdmin>
+
+            </CardAdmin>
+          </div>
+        </section>
+
+
+
+
+
+
+            {/* <CardAdmin>
+                <div className='gird grid-cols-1' >
+                    <LineChart/>
+                </div>
+            </CardAdmin> */}
 
     </Sidebaradmin>
   )
