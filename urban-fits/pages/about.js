bd77dc0b27dc452965e11019d1d5f4e5ad9f4387
Dark Reaper <binarshadsaad@gmail.com>
2023-05-26 17:19:03 +0500

    26-5-2023

--- urban-fits/pages/about.js
+++ urban-fits/pages/about.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState} from 'react';
 import dynamic from 'next/dynamic';
-// const AboutComponent = dynamic(() => import('@/components/aobutComponent'), {
-//     loading: () => <div>Loading...</div>,
-//   });
-import AboutComponent from '@/components/aobutComponent';
+const AboutComponent = dynamic(() => import('@/components/aobutComponent'), {
+    loading: () => <div>Loading...</div>,
+  });
+// import AboutComponent from '@/components/aobutComponent';
 
 const About = () => {
     return (
         <AboutComponent />
     );
 };
-
 export default About;
