43b3978fa0d6c6a33aba8a0bd20f172f513b9667
Dark Reaper <binarshadsaad@gmail.com>
2023-05-27 19:45:19 +0500

    27-5-2023

--- urban-fits/components/aboutComponent.js
+++ urban-fits/components/aboutComponent.js
@@ -1,8 +1,13 @@
-import React, { useEffect, useLayoutEffect, useRef } from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import CustomCursor from "@/components/customCursor";
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import MusicBars from "@/components/musicBars";
 gsap.registerPlugin(ScrollTrigger);
 import Image from 'next/image';
+import Logo from "@/public/logos/logo_black.svg"
 //Images from S3 Bucket
 const colImg1 = "https://urban-fits.s3.eu-north-1.amazonaws.com/aobut-us/aobut-us-img-1.jpg"
 const colImg2 = "https://urban-fits.s3.eu-north-1.amazonaws.com/aobut-us/aobut-us-img-2.jpg"
@@ -22,10 +27,15 @@ const AboutComponent = () => {
     const brand_subhead = useRef()
     const wraperRef = useRef()
     const section_2 = useRef()
+    const section_3 = useRef()
+    const section_4 = useRef()
     const imgCols = useRef()
     const Col1 = useRef()
     const Col2 = useRef()
     const Col3 = useRef()
+    const ufH1 = useRef()
+    const ufH2 = useRef()
+    const ufH3 = useRef()
     const blackBox = useRef()
     const blackBox_whiteSec = useRef()
     const blackBox_blackSec1 = useRef()
@@ -94,30 +104,97 @@ const AboutComponent = () => {
                     },
                     scrollTrigger: {
                         trigger: section_2.current,
-                        start: "bottom end",
-                        // markers: true,
+                        start: "bottom top",
                         scrub: 1.5,
                     }
                 })
-            // .to(blackBox_whiteSec.current, {
-            //     scrollTrigger: {
-            //         trigger: section_2.current,
-            //         pin: true,
-            //         start: "top top",
-            //         // end: "bottom 20%",
-            //         markers: true,
-            //         scrub: 2
-            //     }
-            // })
-
+                .to(blackBox.current, {
+                    yPercent: 0,
+                    scrollTrigger: {
+                        trigger: section_3.current,
+                        start: "top top",
+                        scrub: 1.5
+                    }
+                })
+                // .fromTo(blackBox_whiteSec.current, { height: 0 }, {
+                //     // height: "33.3%",
+                //     scrollTrigger: {
+                //         trigger: section_3.current,
+                //         start: "top top",
+                //         end: "top top",
+                //         // markers: true,
+                //         pin: true,
+                //         scrub: 1.5
+                //     }
+                // })
+                .fromTo(ufH1.current, { opacity: 0, transform: "translateY(100%)" }, {
+                    yPercent: -100,
+                    opacity: 1,
+                    scrollTrigger: {
+                        trigger: section_3.current,
+                        start: "top top",
+                        end: "top top",
+                        scrub: 2
+                    }
+                })
+                .fromTo(ufH2.current, { opacity: 0, transform: "translateY(-100%)" }, {
+                    yPercent: 100,
+                    opacity: 1,
+                    scrollTrigger: {
+                        trigger: section_3.current,
+                        start: "top top",
+                        end: "top top",
+                        scrub: 2,
+                        onEnter: () => {
+                            ufH1.current.classList.add("animate_heading")
+                            ufH2.current.classList.add("animate_heading")
+                            ufH3.current.classList.add("animate_white_heading")
+                        },
+                        onEnterBack: () => {
+                            ufH1.current.classList.remove("animate_heading")
+                            ufH2.current.classList.remove("animate_heading")
+                            ufH3.current.classList.remove("animate_white_heading")
+                        }
+                    }
+                })
+                .to(blackBox_whiteSec.current, {
+                    height: "100%",
+                    scrollTrigger: {
+                        trigger: section_3.current,
+                        start: "bottom bottom",
+                        pin: true,
+                        scrub: 1.5
+                    }
+                })
+                .to([blackBox_blackSec1.current, blackBox_blackSec2.current], {
+                    height: 0,
+                    scrollTrigger: {
+                        trigger: section_3.current,
+                        start: "bottom bottom",
+                        scrub: 1.5
+                    }
+                })
+                .to(section_3.current, {
+                    opacity: 0,
+                    scrollTrigger: {
+                        trigger: section_3.current,
+                        start: "center center ",
+                        scrub: 1.5
+                    }
+                })
         }, wraperRef.current)
         return () => { ctx.revert() }
     }, [])
     return (
         <>
+            <Head>
+                <title>About Us - UF</title>
+            </Head>
+            <MusicBars />
+            <CustomCursor optimize={true} />
             <main className='overflow-hidden'>
 
-                <section ref={wraperRef} className='section_1 w-[400vw] h-[100vh] flex justify-start overflow-y-hidden overflow-hidden'>
+                <section ref={wraperRef} className='relative w-[300vw] h-[100vh] flex justify-start overflow-y-hidden overflow-hidden'>
 
                     <section className="brand w-[58vw] h-screen flex justify-center items-center">
                         <div ref={brand} className="flex flex-col justify-end items-end">
@@ -169,14 +246,33 @@ const AboutComponent = () => {
                         </div>
                     </section>
 
-                    <section ref={blackBox} className="fixed text-white -bottom-full w-full h-screen bg-black flex flex-col justify-center ">
-                        Helllooo
-                        <div ref={blackBox_blackSec1} className="w-full h-1/3 bg-black">
-                            <h1 className="w-full font_copper_gothic text-5xl text-white">Urban Fits&Urban Fits</h1>
+                </section>
+                <section ref={blackBox} className="fixed top-0 translate-y-full text-white w-full h-full bg-black flex flex-col justify-center">
+                </section>
+                <section ref={section_3} className="w-screen h-screen bg-black text-white flex flex-col justify-between overflow-hidden">
+                    <div ref={blackBox_blackSec1} className="w-full h-1/3 justify-self-start self-start bg-black flex justify-center items-center text-white overflow-hidden">
+                        <h1 ref={ufH1} className="w-full font_copper_gothic text-8xl text-white whitespace-nowrap">Urban Fits&nbsp;&nbsp;Urban Fits&nbsp;&nbsp;Urban Fits&nbsp;&nbsp;Urban Fits&nbsp;&nbsp;Urban Fits&nbsp;&nbsp;Urban Fits</h1>
+                    </div>
+
+                    <div ref={blackBox_whiteSec} className="w-full h-0 justify-self-center bg-white overflow-hidden flex justify-center items-center text-black">
+                        <h1 ref={ufH3} className="w-full font_copper_gothic text-8xl text-black whitespace-nowrap">Urban Fits&nbsp;&nbsp;Urban Fits&nbsp;&nbsp;Urban Fits&nbsp;&nbsp;Urban Fits&nbsp;&nbsp;Urban Fits&nbsp;&nbsp;Urban Fits</h1>
+                    </div>
+
+                    <div ref={blackBox_blackSec2} className="w-full h-1/3 justify-self-end self-end bg-black flex justify-center items-center overflow-hidden">
+                        <h1 ref={ufH2} className="w-full font_copper_gothic text-8xl text-white whitespace-nowrap">Urban Fits&nbsp;&nbsp;Urban Fits&nbsp;&nbsp;Urban Fits&nbsp;&nbsp;Urban Fits&nbsp;&nbsp;Urban Fits&nbsp;&nbsp;Urban Fits</h1>
+                    </div>
+                </section>
+                <section ref={section_4} className="bg-white w-screen h-screen flex flex-col justify-between items-center">
+                    <nav className="w-full h-[70px] px-10 flex justify-between items-center">
+                        <Link href="/" ><Image src={Logo} alt='UF' className='w-[40px]' /></Link>
+                        <div className="w-1/5 flex justify-between items-center">
+                            <Link href="/products/Men" className='font_gotham_medium text-base' >Men</Link>
+                            <Link href="/products/Women" className='font_gotham_medium text-base' >Women</Link>
+                            <Link href="/products/Kids" className='font_gotham_medium text-base' >Kids</Link>
                         </div>
-                        <div ref={blackBox_whiteSec} className="w-full bg-white"></div>
-                        <div ref={blackBox_blackSec2} className="w-full h-1/3 bg-black"></div>
-                    </section>
+                    </nav>
+                    <h1 className="font_copper_gothic text-9xl text-center hover:tracking-expand transition-all duration-700"><Link href='/' >URBAN FITS</Link></h1>
+                    <div className="w-full h-20 flex justify-center items-start font_gotham text-center">Urban Fits LLC, State ID: 7053037, Registered Office Address - 500 4th St NW Suite 102 PMB 1958 Albuquerque, NM 87 102 <br />Urban Fits LLC &copy; 2023-2024 all rights reserved</div>
                 </section>
             </main>
         </>
--- urban-fits/pages/about.js
+++ urban-fits/pages/about.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState} from 'react';
 import dynamic from 'next/dynamic';
-const AboutComponent = dynamic(() => import('@/components/aobutComponent'), {
+const AboutComponent = dynamic(() => import('@/components/aboutComponent'), {
     loading: () => <div>Loading...</div>,
   });
 // import AboutComponent from '@/components/aobutComponent';
