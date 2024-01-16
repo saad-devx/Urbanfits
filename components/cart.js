10c54042fcb67d56820ce9e522b13d7d7737eb49
faizan <faizan@urbansoftware.tech>
2024-01-16 19:51:12 +0500

    ui: updated the checkout page coupon system ui debug: added patches scripts for pusher web library

--- components/cart.js
+++ components/cart.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useCart } from "react-use-cart";
 import LinkBtn from '@/components/buttons/link_btn';
 import Button from './buttons/simple_btn';
