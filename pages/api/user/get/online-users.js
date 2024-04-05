fedc1b93c37b2af6de6128b89db70402f82a7f22
faizan <faizan@urbansoftware.tech>
2024-04-05 05:04:32 +0500

    removed unused API endpoints on deprecated Guest user.

--- pages/api/user/get/online-users.js
+++ pages/api/user/get/online-users.js
@@ -1,12 +1,10 @@
 import ConnectDB from "@/utils/connect_db"
 import User from "@/models/user";
-import GuestUser from "@/models/guest_user";
 import StandardApi from "@/middlewares/standard_api";
 
 const getManyUsers = async (req, res) => StandardApi(req, res, { verify_admin: true }, async () => {
     await ConnectDB()
     const usersCount1 = await User.countDocuments({ is_active: true })
-    const usersCount2 = await GuestUser.countDocuments()
 
     res.status(200).json({
         success: true,
